2336fba6468d628df7c800971804ff5fb63844ff
Alex <amhughes@g.cofc.edu>
2016-02-24 12:00:57 -0500

    Initial Import

---
+++ src/com/theironyard/User.java
@@ -0,0 +1,16 @@
+package com.theironyard;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alexanderhughes on 2/24/16.
+ */
+public class User {
+    String name;
+    String password;
+
+    public User(String password, String name) {
+        this.password = password;
+        this.name = name;
+    }
+}
